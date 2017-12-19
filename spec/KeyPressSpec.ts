928e056de18f0c207160a8388b52c9b9d7531b98
Jannes <jannes.meyer@gmail.com>
2017-12-19 22:06:05 +0100

    Add tests

--- spec/KeyPressSpec.ts
+++ spec/KeyPressSpec.ts
@@ -1,7 +1,16 @@
-import KeyPress from '..';
+import KeyPress from '../keypress-tool';
 
-describe('Object equality', () => {
-	it('does something', () => {
-		expect(true).toBe(true);
+describe('KeyPress wrapper', () => {
+	it('returns same bucket with same options', () => {
+		let a = KeyPress('A', 'ctrl');
+		let b = KeyPress('A', 'ctrl');
+		expect(a).toBe(b);
+	});
+	it('can add listeners', () => {
+		let a = KeyPress('A', 'ctrl');
+		a.addListener(function() {
+			console.log('Success');
+		});
+		expect(a.listeners.length).toBe(1);
 	});
 });
